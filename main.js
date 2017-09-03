3ccf0c0372fede271f7a30c302710b6e41232b70
dbublil <bublildan@gmail.com>
2017-09-03 23:13:27 +0300

    fix

--- main.js
+++ main.js
@@ -49,6 +49,31 @@ app.post('/register', function(req, res, next) {
     }
 });
 
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+});
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.send(200);
+    }
+    else {
+        //move on
+        next();
+    }
+});
+
+
 app.post('/login', function(req, res,next) {
     var userName = req.body.username;
     var password = userNamesToPasswords[userName];
